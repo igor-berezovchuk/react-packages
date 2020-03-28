2305e21ff9d78f776e47b3bb50bed1e3dbd6bbc4
Igor Berezovchuk <chev5@ukr.net>
2020-03-28 02:28:33 +0200

    Updates

---
+++ precache-manifest.541e8c5462c3840f43c9535e091c2059.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "958cb94b26f0fba2f702991782c55d18",
+    "url": "/react-packages/index.html"
+  },
+  {
+    "revision": "e34c367890ecf5f0208b",
+    "url": "/react-packages/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "4cc7fd114a6ba4b0a932",
+    "url": "/react-packages/static/js/2.a9783c10.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-packages/static/js/2.a9783c10.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e34c367890ecf5f0208b",
+    "url": "/react-packages/static/js/main.e7f6231d.chunk.js"
+  },
+  {
+    "revision": "b1d8dad43da93a19370a",
+    "url": "/react-packages/static/js/runtime-main.c5d6955a.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/react-packages/static/media/logo.5d5d9eef.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.6c83ad6966531e1eb12a5da20c63fc97.js"
+  "/react-packages/precache-manifest.541e8c5462c3840f43c9535e091c2059.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/react-packages/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
